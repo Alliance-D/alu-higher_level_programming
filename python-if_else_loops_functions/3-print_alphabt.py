111b3e05b1c685efa786e796a28742be35bd9b38
Alliance-D <dushimezi@alustudent.com>
2024-03-01 10:32:12 -0800

    python-if_else_loops_functions

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # prints alphabetical letter except q and e
 for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print("".join(chr(i)), end=" ")
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
