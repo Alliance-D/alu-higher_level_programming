75921af34033d0cc717a23a7c7b5ab0e842bcb65
Alliance-D <dushimezi@alustudent.com>
2024-03-01 08:52:10 -0800

    python-if_else_loops_functions

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-#prints possible combinations of 2 digit numbers
+# prints possible combinations of 2 digit numbers
 for i in range(10):
-        for j in range(i + 1, 10):
-            if i != 8 or j !=9 and i != j:
-               print(f"{i}{j}", end=", ")
-            else:
-                print(f"{i}{j}\n")
+    for j in range(i + 1, 10):
+        if i != 8 or j != 9 and i != j:
+            print("{}{}".format(i,j), end=", ")
+        else:
+            print("{}{}".format(i,j))
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 # prints string in uppercase
+uppercase_str = ""
 def uppercase(str):
     for c in str:
         if ord('a') <= ord(c) <= ord('z'):
-            print("{}".format(chr(ord(c) - 32), end="")
+            uppercase_str += chr(ord(c) - 32)
         else:
-            print()
+            uppercase_str += c
+    print("{}".format(uppercase_str))
