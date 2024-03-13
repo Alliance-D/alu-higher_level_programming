d3014c0487836c37338ef47b2e49230e89ebd5cc
Alliance-D <dushimezi@alustudent.com>
2024-03-13 03:54:48 -0700

    python-classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,7 +3,12 @@
 
 
 class Sqaure:
-    """class that defines properties of Sqaure by Attribute size"""
+    """class that defines properties of Sqaure by Attribute size
+
+     Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
-        """creates instances of Square"""
+        """creates instances of Square
+        """
         self.size = size
