51bbc03d67c039849fea17a703d0e5764340c137
Alliance-D <a.dushimezi@alustudent.com>
2024-03-23 12:34:40 +0000

    python-network_1

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+cript that takes in a URL, sends a request to the URL and dsiplays
+    value of the variable X-Request-Id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    print("{}".format(r.headers['X-Request-Id']))
