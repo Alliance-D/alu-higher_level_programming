ccb995c63120b09ff7ce535a841c177e2dbdad1c
Alliance-D <dushimezi@alustudent.com>
2024-03-23 03:44:43 -0700

    SQL_more_queries

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists all privileges of the MySQL users
 
-GRANT SELECT, INSERT ON user_2_db.* TO user_0d_1@localhost;
-GRANT SELECT, INSERT ON user_2_db.* TO user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
