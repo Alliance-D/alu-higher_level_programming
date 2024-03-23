f9be078b185c13845a5d6f28f4b98e8870eb1fa6
Alliance-D <a.dushimezi@alustudent.com>
2024-03-23 13:47:27 +0000

    python-network_1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -6,6 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alu-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    req = requests.get("https://alu-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
