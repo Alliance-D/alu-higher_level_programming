6521c9cf2e722575663926ad4f90abb6c83c95b7
Alliance-D <dushimezi@alustudent.com>
2024-03-23 04:43:23 -0700

    python-network_0

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes URL as arg sends GET request with additional info
-curl -sSL -H "X-HolbertonSchool-User-Id: 98" "$1
+curl -sSL -H "X-HolbertonSchool-User-Id: 98" "$1"
