b81d5dd3eebb6773ba58c4de727f90a0e50a0880
Alliance-D <a.dushimezi@alustudent.com>
2024-06-12 14:24:43 -0700

    javascript-web_scraping

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    let counter = 0;
-    const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
-      }
-    }
-    console.log(counter);
+//  first argument is the API URL
+const URL = process.argv[2];
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering the
+    // result of the API
+    const json = JSON.parse(body);
+    const charFilms = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
   }
 });
